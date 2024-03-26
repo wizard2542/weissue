be8d508f762f25a82aceb5acf1b76117b01898cf
Kraisorn <kraisorn.c@chareontut.co.th>
2024-03-26 11:01:15 +0700

    Add sideBar
    -Add SideBar
    -Change grid for main card

--- src/components/issuePanel/mainCard.svelte
+++ src/components/issuePanel/mainCard.svelte
@@ -2,111 +2,70 @@
 	import {
 		AccordionItem,
 		Accordion,
-		Button,
-		Textarea,
-		Card,
-		Toolbar,
-		ToolbarGroup,
-		ToolbarButton,
+		Sidebar,
+		SidebarDropdownItem,
+		SidebarDropdownWrapper,
+		SidebarGroup,
+		SidebarItem,
+		SidebarWrapper,
 		Gallery
 	} from 'flowbite-svelte';
-	import { ArrowRightOutline } from 'flowbite-svelte-icons';
-	import { CodeOutline, FaceGrinOutline } from 'flowbite-svelte-icons';
+
 	const items = Array(3);
+	let spanClass = 'flex-1 ms-3 whitespace-nowrap';
+
+  let activeClass = 'flex items-center p-2 text-base font-normal bg-primary-700 rounded-lg text-white hover:bg-gray-700';
+  let nonActiveClass = 'flex items-center p-2 text-base font-normal rounded-lg hover:bg-green-700';
+  
 	const open_all = () => items.forEach((_, i) => (items[i] = true));
 	const close_all = () => items.forEach((_, i) => (items[i] = false));
+
+	import SubCard from './subCard.svelte';
 </script>
 
 <!-- <Button on:click={open_all}>Open all</Button>
 <Button on:click={close_all}>Close all</Button> -->
-<Accordion multiple>
-	<AccordionItem bind:open={items[0]} color="pink">
-		<span slot="header">HB Lot 1 </span>
+<Gallery class="grid grid-cols-6 " >
+	<Sidebar {activeClass} {nonActiveClass} class="bg-gray-700">
+			<SidebarGroup >
+				<SidebarItem class="font-bold" style="color: #ffffff;" label="Dashboard"></SidebarItem>
+				
+				<SidebarItem class="font-bold" style="color: #ffffff;" label="Kanban" {spanClass}>
+					<svelte:fragment slot="subtext">
+						<span
+							class="ms-3 inline-flex items-center justify-center rounded-full bg-gray-200 px-2 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300"
+						>
+							Pro
+						</span>
+					</svelte:fragment>
+				</SidebarItem>
+				<!-- <SidebarItem label="Inbox" {spanClass}>
+					<svelte:fragment slot="subtext">
+						<span
+							class="ms-3 inline-flex h-3 w-3 items-center justify-center rounded-full bg-primary-200 p-3 text-sm font-medium text-primary-600 dark:bg-primary-900 dark:text-primary-200"
+						>
+							3
+						</span>
+					</svelte:fragment>
+				</SidebarItem> -->
+				<SidebarItem class="font-bold" style="color: #ffffff;" label="Users"></SidebarItem>
+				<SidebarItem class="font-bold" style="color: #ffffff;" label="Products"></SidebarItem>
+				<SidebarItem class="font-bold" style="color: #ffffff;" label="Sign In"></SidebarItem>
+				<SidebarItem class="font-bold" style="color: #ffffff;" label="Sign Up"></SidebarItem>
+			</SidebarGroup>
+	</Sidebar>
 
-		<p class="mb-2 text-gray-500 dark:text-gray-400">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-			explicabo ...
-		</p>
-
-		<Accordion multiple>
-			<AccordionItem bind:open={items[4]}>
-				<span slot="header">Change Main</span>
-				<p class="mb-2 text-gray-500 dark:text-gray-400">
+	<Accordion multiple shadow="true" class="col-span-5" activeClass="bg-primary-600 text-white focus:ring-4 focus:ring-blue-800" inactiveClass="text-white hover:bg-white hover:text-gray-800">
+		<AccordionItem bind:open={items[4]}>
+			<span slot="header"><b>Change Main</b></span>
+			<p class="m-2" style="color: #ffffff;">
+				<b>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
 					explicabo ...
-				</p>
-
-				<Accordion multiple>
-					<AccordionItem bind:open={items[6]}>
-						<span slot="header">Issue 1</span>
-						<p class="mb-2 text-gray-500 dark:text-gray-400">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-							explicabo ...
-						</p>
-						<Gallery class="gap-4 grid-cols-2 ">
-							<Card>
-								<Gallery class="grid-cols-2">
-									<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-										Request
-									</h5>
-
-									<ToolbarButton color="indigo" name="Format code"><CodeOutline /></ToolbarButton>
-								</Gallery>
-
-								<Gallery class="gap-4 grid-cols-1">
-									<Textarea id="textarea-id" placeholder="Your message" rows="8" name="message" />
-								</Gallery>
-							</Card>
-
-							<Card>
-								<Gallery class="grid-cols-2">
-									<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-										Response
-									</h5>
-
-									<ToolbarButton color="indigo" name="Format code"><CodeOutline /></ToolbarButton>
-								</Gallery>
-
-								<Gallery class="gap-4 grid-cols-1">
-									<Textarea id="textarea-id" placeholder="Your message" rows="8" name="message" />
-								</Gallery>
-							</Card>
-						</Gallery>
-					</AccordionItem>
-					<AccordionItem bind:open={items[7]}>
-						<span slot="header">Issue 2</span>
-						<p class="mb-2 text-gray-500 dark:text-gray-400">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-							explicabo ...
-						</p>
-					</AccordionItem>
-				</Accordion>
-			</AccordionItem>
-			<AccordionItem bind:open={items[5]}>
-				<span slot="header">IR IDD RUC</span>
-				<p class="mb-2 text-gray-500 dark:text-gray-400">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-					explicabo ...
-				</p>
-			</AccordionItem>
-		</Accordion>
-	</AccordionItem>
-
-	<AccordionItem bind:open={items[1]}>
-		<div slot="header">My Header 2</div>
-		<p class="mb-2 text-gray-500 dark:text-gray-400">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-			explicabo ...
-		</p>
-		<p class="mb-2 text-gray-500 dark:text-gray-400">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
-			explicabo ...
-		</p>
-		<p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-	</AccordionItem>
-
-	<AccordionItem bind:open={items[2]}>
-		<div slot="header">My Header 3</div>
-		<p>Something more</p>
-	</AccordionItem>
-</Accordion>
+				</b>
+			</p>
+			<SubCard></SubCard>
+		</AccordionItem>
+		
+	</Accordion>
+</Gallery>
---
+++ src/components/issuePanel/subCard.svelte
@@ -0,0 +1,58 @@
+<script>
+	import {
+		AccordionItem,
+		Accordion,
+		Textarea,
+		Card,
+		Gallery,GradientButton
+	} from 'flowbite-svelte';
+	import { CodeOutline } from 'flowbite-svelte-icons';
+	const items = Array(3);
+	const open_all = () => items.forEach((_, i) => (items[i] = true));
+	const close_all = () => items.forEach((_, i) => (items[i] = false));
+</script>
+
+
+
+<Accordion multiple shadow="true" rounded="true" border="true" activeClass="bg-white text-gray-800 focus:ring-4 focus:ring-blue-800" inactiveClass="text-gray-800 bg-white hover:bg-primary-600 hover:text-white" >
+    <AccordionItem bind:open={items[6]} >
+        <span slot="header" class=" font-bold">Issue 1</span>
+        <p class="m-4 font-bold " style="color: #ffffff;">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint
+            explicabo ...
+        </p>
+        <Gallery  class="gap-4 grid-cols-1 md:grid-cols-2 font-bold m-4">
+            <Card shadow="true" color="orange" size='lg' style="background-color: #ffb1a5;">
+                <Gallery class="grid-cols-3 mb-2">
+                    <h5 class=" text-2xl tracking-tight">
+                        Request
+                    </h5>
+                    <div></div>
+
+                    <GradientButton   shadow="pink" color="pinkToOrange" name="Format code"><CodeOutline /></GradientButton>
+                </Gallery>
+
+                <Gallery class="gap-4 grid-cols-1">
+                    <Textarea id="textarea-id" placeholder="Your message" rows="8" name="message" />
+                </Gallery>
+            </Card>
+            <Card shadow="true" size='lg' style="background-color: #42b883; color: #000000">
+
+                <Gallery class="grid-cols-3 mb-2">
+                    <h5 class=" text-2xl tracking-tight">
+                        Response
+                    </h5>
+                    <div></div>
+
+                    <GradientButton   shadow="pink" color="pinkToOrange" name="Format code"><CodeOutline /></GradientButton>
+                </Gallery>
+
+                <Gallery class="gap-4 grid-cols-1">
+                    <Textarea id="textarea-id" placeholder="Your message" rows="8" name="message" />
+                </Gallery>
+
+            </Card>
+        </Gallery>
+    </AccordionItem>
+
+</Accordion>
